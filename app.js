0820553ebe0908ce82dc293d617adb0e582d0182
Yuya Ogawa <yuya.ogw@gmail.com>
2022-09-05 07:13:18 +0900

    Fix links

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(null, {
     swaggerOptions: {
-      url: "http://localhost:3000/api-docs",
+      url: "https://localhost:3000/api-docs",
     },
   })
 );
@@ -46,7 +46,7 @@ app.use(
   '/docs',
   redoc({
     title: 'API Docs',
-    specUrl: 'http://localhost:3000/api-docs'
+    specUrl: 'https://localhost:3000/api-docs'
   })
 );
 
